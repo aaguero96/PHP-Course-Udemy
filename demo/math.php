68aa695ee0d9410ecf6e5a35314c78510e6fa34c
aaguero96 <andre.luiz_1996@hotmail.com>
2022-07-12 10:52:28 -0300

    feat: math in php example

---
+++ demo/math.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <?php
+    // sum
+    echo 56 + 45;
+    // other line
+    echo "<br>";
+    // subtraction
+    echo 56 - 45;
+    echo "<br>";
+    // multiplication
+    echo 56 * 45;
+    echo "<br>";
+    // division
+    echo 56 / 45;
+    echo "<br>";
+  ?>
+</body>
+</html>
