024a97bc745ee8faf6fa4daaed60d3b20a95ca6e
aaguero96 <andre.luiz_1996@hotmail.com>
2022-07-14 10:53:51 -0300

    feat: scope in php example

---
+++ demo/scope.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <?php
+    $x = "outside"; //global
+    function convert(){
+      // $x = "inside"; // local
+      global $x;
+      $x = "inside"; // global
+    }
+    echo $x;
+    convert();
+    echo $x;
+  ?>
+</body>
+</html>
