edbc761393513f014cc4a7e0b559ba97819090a4
aaguero96 <andre.luiz_1996@hotmail.com>
2022-07-14 10:52:24 -0300

    feat: switch in php example

---
+++ demo/switch_statements.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <?php
+    $number = 35;
+    switch($number){
+      case 34:
+      echo "its 34";
+      break;
+      case 4:
+      echo "its 4";
+      break;
+      default:
+      echo "its anything";
+      break;
+    }
+  ?>
+</body>
+</html>
