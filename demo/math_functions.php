a4d067da6d27ee1805d75f7353acf912b99b0cdf
aaguero96 <andre.luiz_1996@hotmail.com>
2022-07-14 11:12:08 -0300

    feat: elements functions in php example

---
+++ demo/math_functions.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <?php
+    echo pow(2,7) . "<br>";
+    const min = 0;
+    const max = 100;
+    echo rand(min, max) . "<br>";
+    echo sqrt(500) . "<br>";
+    echo ceil(49.27) . "<br>";
+    echo floor(49.27) . "<br>";
+  ?>
+</body>
+</html>
