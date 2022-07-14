86ce4d8a364e3c4e86df422bc6fea1723566d65b
aaguero96 <andre.luiz_1996@hotmail.com>
2022-07-14 10:52:58 -0300

    feat: if in php example

---
+++ demo/if.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <?php
+    if(3 > 10){
+      echo "three is less than ten";
+    } elseif (5 < 10) {
+      echo "five is less than ten";
+    } else {
+      echo "it is not";
+    }
+  ?>
+</body>
+</html>
